f1917b438c4bcf428a40d48a67110119145b3bf3
Brian V. Hughes <brianvh89@gmail.com>
2016-07-20 11:10:42 -0700

    Configure RSpec and add placeholder main lib file

---
+++ spec/spec_helper.rb
@@ -0,0 +1,39 @@
+ENV['RACK_ENV'] ||= 'test'
+
+require_relative '../config/environment'
+
+FactoryGirl.find_definitions
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.disable_monkey_patching!
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with :truncation
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
